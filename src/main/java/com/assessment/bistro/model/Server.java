808304d441c53e14fffd7b48f5a3a238f9113aa8
Abrar Jaigirdar <jaigirdar111@gmail.com>
2025-02-13 23:48:07 -0500

    Added equals and hashcode in Server model class

--- src/main/java/com/assessment/bistro/model/Server.java
+++ src/main/java/com/assessment/bistro/model/Server.java
@@ -4,13 +4,14 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Table(name = "`Server`")
 public class Server implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int ServerID;
+    private int ServerId;
     private String FirstName;
     private String LastName;
     @Temporal(TemporalType.DATE)
@@ -29,12 +30,12 @@ public class Server implements Serializable {
         TermDate = termDate;
     }
 
-    public int getServerID() {
-        return ServerID;
+    public int getServerId() {
+        return ServerId;
     }
 
-    public void setServerID(int serverID) {
-        ServerID = serverID;
+    public void setServerId(int serverId) {
+        ServerId = serverId;
     }
 
     public String getFirstName() {
@@ -72,11 +73,23 @@ public class Server implements Serializable {
     @Override
     public String toString() {
         return "Server{" +
-                "ServerID=" + ServerID +
+                "ServerID=" + ServerId +
                 ", FirstName='" + FirstName + '\'' +
                 ", LastName='" + LastName + '\'' +
                 ", HireDate=" + HireDate +
                 ", TermDate=" + TermDate +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Server server)) return false;
+        return getServerId() == server.getServerId() && Objects.equals(getFirstName(), server.getFirstName()) && Objects.equals(getLastName(), server.getLastName()) && Objects.equals(getHireDate(), server.getHireDate()) && Objects.equals(getTermDate(), server.getTermDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getServerId(), getFirstName(), getLastName(), getHireDate(), getTermDate());
     }
 }
