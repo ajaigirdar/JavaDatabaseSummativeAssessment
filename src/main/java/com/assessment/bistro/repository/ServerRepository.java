7e50f6da2a85ce5c73b5f92ce7ce9052cb60cdba
Abrar Jaigirdar <jaigirdar111@gmail.com>
2025-02-13 23:49:17 -0500

    Implemented ServerRepository with JpaRepository

---
+++ src/main/java/com/assessment/bistro/repository/ServerRepository.java
@@ -0,0 +1,12 @@
+package com.assessment.bistro.repository;
+
+import com.assessment.bistro.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ServerRepository extends JpaRepository<Server,Integer> {
+
+//    List<Server>
+
+}
